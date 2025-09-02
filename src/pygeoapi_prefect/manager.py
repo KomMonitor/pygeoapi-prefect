1a3ba5be63f142fc11689de8e37643a08b7a79a0
cDanowski <christian.danowski-buhren@hs-bochum.de>
2025-09-02 22:26:15 +0200

    return job_id when triggering schedule execution

--- src/pygeoapi_prefect/manager.py
+++ src/pygeoapi_prefect/manager.py
@@ -346,12 +346,13 @@ class PrefectManager(BaseManager):
             
     def trigger_flow_from_schedule(  # type: ignore [empty-body]
             self, schedule_id: str
-    ) -> bool:
+    ) -> str:
         """Trigger a flow run for schedule."""
         deploy_name = self._schedule_id_to_deploy_name(schedule_id)
 
         try:
-            flow_run_id = anyio.run(_trigger_prefect_flow_run_for_deployment, deploy_name)
+            flow_run = anyio.run(_trigger_prefect_flow_run_for_deployment, deploy_name)
+            job_id = self._flow_run_name_to_job_id(flow_run.name)            
         except ObjectNotFound as err:
             raise ScheduleNotFoundError()
         except httpx.ConnectError as err:
@@ -360,10 +361,10 @@ class PrefectManager(BaseManager):
             logger.error(f"Could not connect to prefect server: {str(err)}")
             return False
         else:
-            if flow_run_id is None:
+            if flow_run is None:
                 raise ScheduleNotFoundError()
             else:
-                return True
+                return job_id
 
     def delete_job(  # type: ignore [empty-body]
             self, job_id: str
@@ -975,6 +976,6 @@ async def _trigger_prefect_flow_run_for_deployment(deployment_name: str) -> Depl
         except IndexError:
             result = None
         else:
-            response = await client.create_flow_run_from_deployment(deployment.id)
-            result = response.data.create_flow_run_by_pk.id
+            flow_run = await client.create_flow_run_from_deployment(deployment.id)
+            result = flow_run
         return result
